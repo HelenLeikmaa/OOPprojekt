58e05bf7b2c04b467e969d738b0df4c816540eaf
HelenLeikmaa <93259798+HelenLeikmaa@users.noreply.github.com>
2022-04-06 15:10:39 +0300

    esimene pühendumine koos projekti klassidega

---
+++ src/Rebane.java
@@ -0,0 +1,38 @@
+import java.lang.Math;
+
+// easter egg
+public class Rebane extends Mängija implements Skill{
+
+    private String riietus = "Rebase kostüüm";
+    private int sneakVäärtus;
+    private int eluVäärtus;
+
+
+    public Rebane(String nimi, int x, int y) {
+        super(nimi);
+        this.sneakVäärtus = x;
+        this.eluVäärtus = y;
+
+    }
+
+    @Override
+    public int sneak(int x) {
+        return 0;
+    }
+
+    @Override
+    public int ellujäämisoskus(int y) {
+        if (y != -1) {
+            return y;
+        }
+        return (int) ((Math.random() * 10) + 0);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\t riietus : " + riietus + '\n' +
+                "\t sneak : " + sneak(sneakVäärtus) + '\n' +
+                "\t ellujäämisoskus : " + ellujäämisoskus(eluVäärtus);
+    }
+}
---
+++ src/Välimus.java
@@ -0,0 +1,22 @@
+public class Välimus{
+
+    // jumpsuit-il on alati värv, aga randoomsega võib see olla ka loom, mille puhul ei ole juuksevärvi
+    public static String jumpsuit(String jumpsuitVärv) {
+        if (jumpsuitVärv.equals("random")){
+            String[] list = {"lilleline", "triibuline", "oranz", "mummuline", "laiguline", "rebane"};
+            String valik = list[(int) ((Math.random() * 6) + 0)];
+            return valik;
+        }
+        return jumpsuitVärv;
+    }
+
+    // kui ei ole loom, siis annab juuksevärvi ka
+    public static String juustevärv(String juusteVärv){
+        if (juusteVärv.equals("random")){
+            String[] list2 = {"roheline", "blond", "punane", "must", "sinine", "brünett"};
+            String valik2 = list2[(int) ((Math.random() * 5) + 0)];
+            return valik2;
+        }
+        return juusteVärv;
+    }
+}

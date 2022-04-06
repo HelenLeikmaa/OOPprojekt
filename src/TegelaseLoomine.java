c04dd42cc10863c7a65323b180cf4cb985d005ab
HelenLeikmaa <93259798+HelenLeikmaa@users.noreply.github.com>
2022-04-06 21:22:32 +0300

    lõin uued tühjad klassid tuleviku tarbeks

--- src/TegelaseLoomine.java
+++ src/TegelaseLoomine.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
-public class Peaklass {
+public class Peaklass extends Mängija {
+
+    public Peaklass(String nimi, String jumpsuitVärvus, String juustevärvus, int sneakVäärtus, int eluväärtus) {
+        super(nimi, jumpsuitVärvus, juustevärvus, sneakVäärtus, eluväärtus);
+    }
+
     public static void main(String[] args) {
 
         // paar väärtust, mida oli vaja enne käima panemist.
@@ -11,21 +16,22 @@ public class Peaklass {
         // scanner võtab kogu info mis antakse ja söödab selle sisse peaklassi
         Scanner info = new Scanner(System.in);
 
-        System.out.println("sisesta oma Nimi");
-        String nimi  = info.nextLine();
+        System.out.println("sisesta oma nimi");
+        String nimi = info.nextLine();
 
         System.out.println("vali jumpsuiti värv: lilleline, triibuline, oranz, mummuline, laiguline, random");
         String valik1 = info.nextLine();
         String jumpsuitValik = Välimus.jumpsuit(valik1);
-        if(valik1.equals("random")){
+        if (valik1.equals("random")) {
             System.out.println("valiti " + jumpsuitValik);
         }
 
+        String valik2 = null;
         if (!jumpsuitValik.equals("rebane")) {
             System.out.println("Vali juuksevärv: roheline, blond, punane, must, sinine, brünett, random");
-            String valik2 = info.nextLine();
+            valik2 = info.nextLine();
             juustevärv = Välimus.juustevärv(valik2);
-            if(valik2.equals("random")){
+            if (valik2.equals("random")) {
                 System.out.println("valiti " + juustevärv);
             }
         }
@@ -35,31 +41,37 @@ public class Peaklass {
 
         String valik = info.nextLine();
 
-        if (valik.equals("jah")){
-            if (jumpsuitValik.equals("rebane")){
-                System.out.println("kuna sa oled rebane, siis su sneak on eos 0 ");
-            }
-            else {
+        if (valik.equals("jah")) {
+            if (jumpsuitValik.equals("rebane")) {
+                sneak = 8;
+                System.out.println("kuna sa oled rebane, siis su sneak on eos 8 ");
+            } else {
                 System.out.println("Sneak : ");
                 sneak = info.nextInt();
             }
             System.out.println("Ellujäämisoskus : ");
             ellujäämine = info.nextInt();
 
-        }
-
-        else {
+        } else {
             if (jumpsuitValik.equals("rebane")) {
-                System.out.println("kuna sa oled rebane, siis su sneak on eos 0 ");
+                System.out.println("kuna sa oled rebane, siis su sneak on eos 8 ");
             }
         }
-        if (jumpsuitValik.equals("rebane")){
-            Rebane mängija = new Rebane(nimi, sneak, ellujäämine);
+        if (jumpsuitValik.equals("rebane")) {
+            Rebane mängija = new Rebane(nimi, jumpsuitValik, juustevärv, sneak, ellujäämine);
             System.out.println(mängija);
-        }
-        else {
+        } else {
             TavalineMängija mängija = new TavalineMängija(nimi, jumpsuitValik, juustevärv, sneak, ellujäämine);
             System.out.println(mängija);
         }
+
+        Peaklass ebatavalineMängija = new Peaklass(nimi, valik1, valik2, sneak, ellujäämine);
+        System.out.println(ebatavalineMängija);
     }
-}
+
+    }
+
+
+
+
+

328b011bcc0cb8f4a3f4a1142245f9059dac2839
HelenLeikmaa <93259798+HelenLeikmaa@users.noreply.github.com>
2022-04-07 17:07:19 +0300

    "Kirjavigade parandamine."

--- src/HeaSneakHeaEllujäämine.java
+++ src/HeaSneakHeaEllujäämine.java
@@ -10,7 +10,7 @@ public class HeaSneakHeaEllujäämine {
     public void esimeneInfoMängijale() {
         List<String> asjad = new ArrayList<>();
         asjad.add("nuga");
-        System.out.println("Tere! Oled jõudnud läbi portaali Maailma. Sul on head hiilimise ja ellujäämise oskused.");
+        System.out.println("Tere! Oled jõudnud läbi portaali Maailma. Sul on head hiilimise ja ellujäämise oskused. Hea algus!");
         System.out.println("Avastad, et sul on kaasas " + asjad.toString());
 
     }

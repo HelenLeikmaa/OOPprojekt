33336496abe63120ede499fa51f03c742b442e41
HelenLeikmaa <93259798+HelenLeikmaa@users.noreply.github.com>
2022-05-19 13:40:41 +0300

    "Ma ei ole päris kindel, mis juhtus, aga mh on graafika kaks esimest lehte poolvalmis."

--- graafika/src/main/java/com/example/graafika/Aken.java
+++ graafika/src/main/java/com/example/graafika/Aken.java
@@ -1,10 +1,13 @@
 package com.example.graafika;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -12,42 +15,48 @@ import javafx.stage.Stage;
 public class Aken extends Application {
     @Override
     public void start(Stage aken) {
-        Label küsinNime = new Label("Sisesta enda tegelase nimi: ");
-        TextField sisestaNimi = new TextField();
 
-        Label küsinTunked = new Label("Milliseid tunkesid tahad?");
+        //esimene aken//
+        Label küsinSuvaliselt = new Label("Sisesta vabalt valitud sõna (et saaksid mängimise jaoks sõrmed soojaks): ");
+        TextField vastabSuvaliselt = new TextField();
+
+        //teine aken//
+        Label küsinEkstistentsiaalseKüsimuse = new Label("Miks oled siin?");
         final ToggleGroup nupugrupp = new ToggleGroup();
-        RadioButton tunked1 = new RadioButton("lillelisi");
-        tunked1.setSelected(true);
-        tunked1.setToggleGroup(nupugrupp);
-        RadioButton tunked2 = new RadioButton("triibulisi");
-        tunked2.setToggleGroup(nupugrupp);
-        RadioButton tunked3 = new RadioButton("oranže");
-        tunked3.setToggleGroup(nupugrupp);
-        RadioButton tunked4 = new RadioButton("mummulisi");
-        tunked4.setToggleGroup(nupugrupp);
-        RadioButton tunked5 = new RadioButton("laigulisi");
-        tunked5.setToggleGroup(nupugrupp);
-        RadioButton tunked6 = new RadioButton("üllata mind!");
-        tunked6.setToggleGroup(nupugrupp);
+        RadioButton vastus1 = new RadioButton("Tahan hirmsasti mängida ^^.");
+        vastus1.setSelected(true);
+        vastus1.setToggleGroup(nupugrupp);
+        RadioButton vastus2 = new RadioButton("Juhendaja sunnib:/");
+        vastus2.setToggleGroup(nupugrupp);
+        RadioButton vastus3 = new RadioButton("Mul on igav.");
+        vastus3.setToggleGroup(nupugrupp);
+        RadioButton vastus4 = new RadioButton("Mind sunnitakse õpilaste töid parandama.");
+        vastus4.setToggleGroup(nupugrupp);
+        RadioButton vastus5 = new RadioButton("Miks üldse keegi siin on? Eksistents on kannatus.");
+        vastus5.setToggleGroup(nupugrupp);
 
         VBox teineLeht = new VBox();
         teineLeht.setSpacing(2);
         teineLeht.setPadding(new Insets(5));
-        teineLeht.getChildren().addAll(küsinTunked, tunked1, tunked2, tunked3, tunked4, tunked5, tunked6);
+        teineLeht.getChildren().addAll(küsinEkstistentsiaalseKüsimuse, vastus1, vastus2, vastus3, vastus4, vastus5);
 
         Button nupp1 = new Button("Valmis!");
         nupp1.setOnMouseClicked(event -> {
-            String nimi = sisestaNimi.getText();
-            RadioButton valitudTunked = (RadioButton) nupugrupp.getSelectedToggle();
-            String tunked = valitudTunked.getText();
+            String suvalineSõna = vastabSuvaliselt.getText();
             aken.setScene(new Scene(teineLeht));
+        });
 
+        vastabSuvaliselt.setOnKeyPressed(k -> {
+            if (k.getCode().equals(KeyCode.ENTER)) {
+                RadioButton valitudTunked = (RadioButton) nupugrupp.getSelectedToggle();
+                String tunked = valitudTunked.getText();
+                aken.setScene(new Scene(teineLeht));
+            }
         });
 
         GridPane esimeneLeht = new GridPane();
-        esimeneLeht.add(küsinNime, 0, 0);
-        esimeneLeht.add(sisestaNimi, 1, 0);
+        esimeneLeht.add(küsinSuvaliselt, 0, 0);
+        esimeneLeht.add(vastabSuvaliselt, 1, 0);
         esimeneLeht.add(nupp1, 0, 2);
         esimeneLeht.setHgap(5);
         esimeneLeht.setVgap(5);
